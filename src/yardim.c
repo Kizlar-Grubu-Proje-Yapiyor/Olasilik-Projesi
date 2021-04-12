01f63334d724eefce174274fbfae283018d82c79
Ceyhun Şen <ceyhunsen237@gmail.com>
2021-04-12 14:22:35 +0300

    tabs, spaces...

--- src/yardim.c
+++ src/yardim.c
@@ -7,11 +7,11 @@ void yardim(FILE *fp, const char *argv)
 	        "Secenekler:\n"
 	        "-y, --yardim\n"
 	        "\tYardimi goruntuler\n"
-			"-s, --sinirlar\n"
-			"\tBagli oldugunuz sinirlari goruntuler\n"
+	        "-s, --sinirlar\n"
+	        "\tBagli oldugunuz sinirlari goruntuler\n"
 	        "-d [DOSYA], --dosya [DOSYA]\n"
 	        "\tGirdiyi belirtilen dosyadan alir\n"
-            "\n"
-            "--aritmetik-ortalama [DEGER(LER)]\n"
+	        "\n"
+	        "--aritmetik-ortalama [DEGER(LER)]\n"
 	        "\tVerilen sayilarin aritmetik ortalamasini hesaplar\n");
 }
