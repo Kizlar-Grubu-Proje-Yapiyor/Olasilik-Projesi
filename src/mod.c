919b5870b05632cfee66ed7036ca93eb8e2d0706
Ceyhun Şen <ceyhunsen237@gmail.com>
2021-04-17 15:00:47 +0300

    mod algoritması güncellendi

--- src/mod.c
+++ src/mod.c
@@ -1,30 +1,24 @@
 #include <limits.h>
-#include <float.h>
 #include "mod.h"
-#include "algoritmalar.h"
 
 double mod(int size, double *arr)
 {
-	d_sort(arr, size);
-	int mx = INT_MIN;
-	int cnt = 0, lasti = 0;
-	double ans = arr[0];
-	for (int i = 1; i < size; i++) {
-		if (arr[i] == arr[i - 1]) {
+	double mx = INT_MIN, ans = 0, cnt;
+
+	for (int i = 0; i < size; i++) {
+		cnt = 0;
+		if (mx == arr[i]) {
 			continue;
-		} else {
-			cnt = i - lasti - 1;
-			if (mx < cnt) {
-				ans = arr[i - 1];
-				mx = cnt;
-			}
-			lasti = i;
+		}
+		for (int j = 0; j < size; j++) {
+			if (arr[i] == arr[j])
+				cnt++;
+		}
+		if (mx < cnt) {
+			mx = cnt;
+			ans = arr[i];
 		}
 	}
-	cnt = size - lasti - 1;
-	if (mx < cnt) {
-		ans = arr[size - 1];
-		mx = cnt;
-	}
+
 	return ans;
 }
