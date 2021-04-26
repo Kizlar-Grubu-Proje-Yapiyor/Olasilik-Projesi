b577a870b9a468bb1123d92ba8975a760d05e9a4
Ceyhun Şen <ceyhunsen237@gmail.com>
2021-04-26 18:49:51 +0300

    interaktif mod için sınırlar düzeltildi

--- src/interaktif.c
+++ src/interaktif.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "interaktif.h"
+#include "main.h"
+#include "sinirlar.h"
 
 double *interaktif(int *size)
 {
@@ -8,6 +10,13 @@ double *interaktif(int *size)
 	double tmp;
 	printf("Diziniz kac elemanli: ");
 	scanf("%d", size);
+	if (*size > MAX_NUM) {
+		fprintf(stderr, "Desteklenen maksimum sayi miktarindan "
+		                "daha fazla sayi girdiniz!\nDesteklenen "
+		                "maksimum sayi miktari: %d\n\n", MAX_NUM);
+		sinirlar(stderr);
+		exit(1);
+	}
 	arr = (double *)malloc(sizeof(double) * (*size));
 	if (!arr) {
 		fprintf(stderr, "Bellek yetersiz!\n");
