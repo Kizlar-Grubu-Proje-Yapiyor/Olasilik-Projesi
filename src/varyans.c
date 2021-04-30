80c48ac1e1cc079fdb031dd3ca1f3bb0bbac17a5
Ceyhun Şen <ceyhunsen237@gmail.com>
2021-04-30 13:20:14 +0300

    varyans.c: çok büyük bir hata düzeltildi

--- src/varyans.c
+++ src/varyans.c
@@ -1,13 +1,14 @@
 #include "varyans.h"
 #include "aritmetikortalama.h"
+#include "main.h"
 #include <math.h>
 
 double varyans(int size, double *arr)
 {
 	double ao = aritmetik_ortalama(size, arr);
+	double arrtmp[MAX_NUM];
 	for (int i = 0; i < size; i++) {
-		arr[i] = arr[i] - ao;
-		arr[i] = pow(arr[i], 2);
+		arrtmp[i] = pow(arr[i] - ao, 2);
 	}
-	return aritmetik_ortalama(size, arr);
+	return aritmetik_ortalama(size, arrtmp);
 }
