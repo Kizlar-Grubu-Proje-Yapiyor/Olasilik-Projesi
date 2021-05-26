23e8d9e8063b2d4da2b780ce843eedaf7ae70d06
Ceyhun Şen <ceyhunsen237@gmail.com>
2021-05-26 15:12:17 +0300

    versiyon bilgisi güncellendi

--- src/versiyon.c
+++ src/versiyon.c
@@ -4,11 +4,11 @@
 void versiyon(FILE *fp)
 {
 	fprintf(fp,
-	        "MAT0293 Olasilik ve Istatistik Projesi v1.4\n\n"
-	        "Bu program GNU General Public License v3.0 ile lisanslanmistir. "
-	        "Daha fazla bilgi icin 'LICENCE' adli dosyayi inceleyiniz.\n\n"
-	        "Bu proje Ali Cuneyt Caglar, Ali Furkan Demir, Ceyhun Sen, Halil "
-	        "Karatas ve Ibrahim Yahya Aydinli tarafindan yapilmistir.\n\n"
+	        "MAT0293 Olasilik ve Istatistik Projesi v1.5\n"
+	        "Bu proje Ali Cuneyt Caglar, Ali Furkan Demir, Ceyhun Sen,\n"
+	        "Halil Karatas ve Ibrahim Yahya Aydinli tarafindan yapilmistir.\n"
+	        "Bu program GNU General Public License v3.0 ile lisanslanmistir.\n"
+	        "Daha fazla bilgi icin 'LICENCE' adli dosyayi inceleyiniz.\n"
 	        "Bursa Teknik Universitesi, 2021\n"
 	        );
 }
